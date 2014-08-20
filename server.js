6348a0019d5557e2c65b5306082ea32ff5799312
Steven Hein <steven.c.hein@gmail.com>
2014-08-20 15:41:46 -0400

    added staging functionality

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
 //be careful with plurals - all the singular words are dealing with one object, and the plurals are dealing with multiple
+//database types:
+//	item
+//	location
+//	staged
+//	history
+
 console.log('server running');
 
 //CONFIG -------------------------------------------------------------------------------------
 //database
 var mongojs = require('mongojs');
 var db = mongojs('mongodb://localhost:27017/itemdb', ['itemdb']);
-
 //app engine
 var express = require('express'),
     app = express();
-
 //app configuration
 app.configure(function(){
     app.use(app.router);
     app.use(express.static(__dirname + '/www'));
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
-
 //email auth
 var nodemailer = require('nodemailer');
 var smtpTransport = require('nodemailer-smtp-transport');
@@ -28,85 +31,104 @@ smtpTrans = nodemailer.createTransport(smtpTransport({
   	}
 }));
 
-
 //API ---------------------------------------------------------------------------------------
 
 //Items  --------------------------------
 app.get('/api/getItems', function (req, res) {
-	db.itemdb.find({type:'item'},function(err, docs) {
-		res.send(docs);
+	db.itemdb.find({type:'item'},function (err, docs) {
+		if(err){ console.log('(error getting items) '+err); }else{ res.send(docs); }
 	});
 });//end GET items
 
 //for an example of this - see "testing calls" bottom of item.js
-app.post('/api/getItem', function (req,res) {
-	db.itemdb.findOne(req.body, function(err, doc){
-		res.send(doc);
+app.post('/api/getItem', function (req, res) {
+	db.itemdb.findOne(req.body, function (err, doc) {
+		if(err){ console.log('(error getting item) '+err); }else{ res.send(doc); }
 	});
 });//end 'GET' (single) item - send the uid and retrieve item (untested - send multiple uid's?)
 
 app.post('/api/saveItem',express.json(), function (req, res) {
-	db.itemdb.insert(req.body);
+	db.itemdb.insert(req.body, function (err, doc) { 
+		if(err){ console.log('(error saving item) '+err); }else{ res.send(doc); } 
+	});
 });//end SAVE single item
 
 app.post('/api/removeItem', express.json(), function (req, res) {
-	db.itemdb.remove(req.body);
+	db.itemdb.remove(req.body, function (err, doc) {
+		if(err){ console.log('(error removing item) '+err); }else{ res.send(doc); }
+	});
 });//end REMOVE single item
 
 app.post('/api/updateItem', express.json(), function (req, res) {
-	db.itemdb.update({uid: req.body.uid}, req.body);
+	db.itemdb.update({uid: req.body.uid}, req.body, function (err, doc) {
+		if(err){ console.log('(error updating item) '+err); }else{ res.send(doc); }
+	});
 });//end UPDATE single item
 
 app.post('/api/stageItem', express.json(), function (req, res) {
-	db.itemdb.insert({type:'staged', key:req.body.actionKey, modifiedItem:req.body.data});
-});//end UPDATE single item
-
-
-
-
-//Item dictionaries --------------------------------
-
-//locations:
-app.get('/api/getLocations', function (req,res) {
-	db.itemdb.find({type:'location'}, function(err, doc){
-		res.send(doc);
+	db.itemdb.insert({type:'staged', key:req.body.actionKey, modifiedItem:req.body.data}, function (err, doc) {
+		if(err){ console.log('(error staging item) '+err); }else{ res.send(doc); }
 	});
-});//end GET locations
-app.post('/api/saveLocation', express.json(), function (req, res) {
-	db.itemdb.insert(req.body);
-});//end SAVE location
+});//end 'STAGE' single item
 
-//form fields:
-//GET form fields....
+app.get('/api/unStage/:key/:decision', function (req, res) {
+	//find the staged item based on key
+	db.itemdb.find({type:'staged', key:req.params.key}, function (err, doc) {
+		//for some reason - it is returning an array......
+		modifiedItem = doc[0].modifiedItem;
+		//slap in the historical item
+		db.itemdb.find({type:'item', uid:modifiedItem.uid}, function (err, old) {
+			db.itemdb.insert({type:'history', forUID:old[0].uid, data:old[0] }, function (err, doc) {});
+		});
+		//update with new item data
+		db.itemdb.update({uid:modifiedItem.uid}, modifiedItem);
+	});
+	//remove the staged item
+	db.itemdb.remove({type:'staged', key:req.params.key}, function (err, doc) {});
+});//end 'UNSTAGE' single item
 
-app.post('/api/saveField', express.json(), function (req, res) {
-	db.itemdb.insert(req.body);
-});//end SAVE form field
-
-
-
-
-//Emails ----------------------------------------
+//EMAILS ----------------------------------------
 
 app.post('/api/sendEmail', express.json(), function (req, res){
 
-	console.log(req.body.to);
+	console.log('trying to send email to ' + req.body.to);
 	smtpTrans.sendMail({
 	    from: 'Robot <colabrobot@gmail.com>',
 	    to: req.body.to,
 	    subject: req.body.subject,
 	    text: 'text body',
 	    html: req.body.HTMLbody
-	}, function(error, info){
-	    if(error){
-	        console.log(error);
-	    }else{
-	        console.log('Message sent: ' + info.response);
-	    }
+	}, function (err, doc){
+	    if(err){ console.log(err); }else{ 
+	    	//email was sent!
+	    	res.send(250);
+	    	console.log('Message sent: ' + doc.response); }
 	});
     
 });
 	
+//DICTIONARIES --------------------------------
+
+//locations:
+app.get('/api/getLocations', function (req,res) {
+	db.itemdb.find({type:'location'}, function (err, doc) {
+		if(err){ console.log(err); }else{ res.send(doc); }
+	});
+});//end GET locations
+app.post('/api/saveLocation', express.json(), function (req, res) {
+	db.itemdb.insert(req.body, function (err, doc) {
+		if(err){ console.log(err); }else{ res.send(doc); }
+	});
+});//end SAVE location
+
+//form fields:
+//GET form fields....
+//app.post('/api/saveField', express.json(), function (req, res) {
+//	db.itemdb.insert(req.body);
+//});//end SAVE form field
+
+
+
 
 
 app.listen(80);
--- www/js/item.js
+++ www/js/item.js
@@ -9,17 +9,17 @@ var emailSet = false;
 //cookies:
 var email = $.cookie('email'); 
 
-//ITEM CONTROLLER -----------------------------------------------------------------
-itemApp.controller('itemCtrl', function($scope, $http) {
+//############################################################################################################################################################
+//ITEM CONTROLLER ############################################################################################################################################################
+//############################################################################################################################################################
 
+itemApp.controller('itemCtrl', function ($scope, $http) {
   //get email
   $scope.email = email;
-  
   //initilize items
   $http.get('/api/getItems').then(function (response) {
     $scope.items = response.data;
   }); //end getItems
-
   //initilize locations
   $scope.locations=['Other (add one)'];
   $http.get('/api/getLocations').then(function (response) {
@@ -28,37 +28,37 @@ itemApp.controller('itemCtrl', function($scope, $http) {
     }
   }); //end getLocations
 
-
-  //ITEM FUNCTIONS ------------------------------------
-  $scope.addItem = function() {
-    
+  //ITEM FUNCTIONS ----------------------------------------------------------------------------------------------------------------------------
+  $scope.addItem = function () {
     //save location if new
     $scope.addLocation();
-
     //set image if no url specified (this could be made as a function - maybe a list of most used images or generic ones?)
     if (($scope.item.image == '')||(typeof $scope.item.image == 'undefined')) { $scope.item.image = "http://s3.timetoast.com/public/uploads/photos/1687876/Unknown-7_small_square.jpeg?1312881542"; }
-      
+    //set 'hidden' fields
     $scope.item['uid'] = generateUID();
     $scope.item['type'] = 'item';
     $scope.item['number'] = $scope.items.length + 1; 
     $scope.item['posted'] = generateDate(); 
     $scope.item['owner'] = $scope.email;
-
-    //add to the ng-repeat and save (probably should be doing this differently - like it adds once it saves...)
-    $scope.items.push($scope.item);
-    $http.post('/api/saveItem', $scope.item);
-
+    //save - then add to the ng-repeat
+    $http.post('/api/saveItem', $scope.item).success(function (data) { 
+      $scope.items.push(data); 
+    });
+    //remove form
     showForm = false;
     delete $scope.item;
-  
-  }; //end addItem ------------------------------------
+  }; //end addItem ---------------
 
-  $scope.removeItem = function(itemUID) {
-    $http.post('/api/removeItem', JSON.stringify({ uid:itemUID }));
-    $('#'+itemUID).css('background-color','#EDE');
-  }; //end removeItem ------------------------------------
+  $scope.removeItem = function (itemUID) {
+    $http.post('/api/removeItem', JSON.stringify({ uid:itemUID })).success(function () {
+      $('#'+itemUID).css('background-color','#EDE');
+    });
+    
+  }; //end removeItem ---------------
 
-  $scope.updateItem = function() {
+  $scope.updateItem = function () {
+    //remove _id
+    delete $scope.item._id;
     //timestamp for update
     $scope.item['updated'] = generateDate();
     //check to see who is editing
@@ -66,91 +66,97 @@ itemApp.controller('itemCtrl', function($scope, $http) {
       //add location if new
       $scope.addLocation();
       $http.post('/api/updateItem', $scope.item);
-
     } else { //stage item if different author (have a function to change ownership - maybe in the body of the email!)  <<<<<<<<<<<<<
-    
       $scope.item['updatedBy'] = $scope.email;
+      
       var key = generateUID();
-      $http.post('/api/stageItem', { actionKey:key, data:$scope.item });
-      $scope.itemChangeEmailGen($scope.item);
 
+      //the following creates a staged item that can be converted or 'unstaged' by a call to /api/unstage/<key>
+      $http.post('/api/stageItem', { actionKey:key, data:$scope.item });
+      $scope.itemChangeEmailGen($scope.item, key);
     }
 
     showForm = false;
     delete $scope.item;
 
-  }; //end updateItem ------------------------------------
+  }; //end updateItem ---------------
 
-  $scope.selectItem = function(fnitem) {
+  $scope.selectItem = function (fnitem) {
     //set the form field bindings
     $scope.item = fnitem;
     showUpdateButton=true;
     showForm=true;
-  }; //end selectItem ------------------------------------
+  }; //end selectItem ---------------
 
-  $scope.checkLocation = function() {
+  //PAGE FUNCTIONS ----------------------------------------------------------------------------------------------------------------------------
+  $scope.setEmailCookie = function () {
+    $.cookie('email', $scope.email);
+  };//end setEmailCookie ---------------
+
+  $scope.addLocation = function () {
+    if ($scope.item.loc == 'Other (add one)') { 
+      $http.post('/api/saveLocation', {type:'location', name:$scope.item.other}).success(function() {
+        $scope.item.loc = $scope.item.other;
+        $scope.item.other = '';
+        $scope.locations.push($scope.item.loc); 
+      });
+    }//end if
+  }; //end addLocation ---------------
+
+  $scope.showForm = function () {
+    return showForm;
+  };//end showForm ---------------
+
+  $scope.showOtherLocationField = function () {
     //this opens the element for new location (could be made better - where should this be placed? might be able to combine with addLocation)
     if (typeof $scope.item =='undefined') { return false; }
     else if($scope.item.loc == 'Other (add one)'){ 
       return true; } 
     else { return false; }
-  }; //end checkLocation ------------------------------------
+  }; //end showOtherLocationField ---------------
 
-  $scope.addLocation = function () {
-    if ($scope.item.loc == 'Other (add one)') { 
-      $http.post('/api/saveLocation', {type:'location', name:$scope.item.other});
-      $scope.item.loc = $scope.item.other;
-      $scope.item.other = '';
-      $scope.locations.push($scope.item.loc); 
-    }
-  }; //end addLocation ------------------------------------
-
-  $scope.showForm = function() {
-    return showForm;
-  };//end showForm ------------------------------------
-
-  $scope.showUpdateButtonFunction = function() {
+  $scope.showUpdateButtonFunction = function () {
     return showUpdateButton;
-  };//end showForm ------------------------------------
+  };//end showUpdateButtonFunction ---------------
 
-  $scope.addItemClick = function() {
+  $scope.addItemClick = function () {
     $scope.item = {};
     showForm = true;
-  };//end addItemClick ------------------------------------
+  };//end addItemClick ---------------
 
-  $scope.cancelForm = function() {
+  $scope.cancelForm = function () {
     showForm = false;
     console.log('canceled form')
-  };//end cancelForm ------------------------------------
-
-  $scope.setEmailCookie = function() {
-    $.cookie('email', $scope.email);
-  };//end setEmailCookie ------------------------------------
+  };//end cancelForm ---------------
 
 
   //remember to send this funtion the entire item - it may get deleted before it can be sent
-  $scope.itemChangeEmailGen = function(itemX) {
+  $scope.itemChangeEmailGen = function (itemX, actionKey) {
     var packageEmail = {};
     packageEmail.to = itemX.owner;
-    packageEmail.subject = '[inventory/ ' + 'changes have been made to ' + itemX.name;
-    packageEmail.HTMLbody = '<!DOCTYPE HTML><html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /></head><body><h3>do you like these changes?</h3><h4>' + itemX.name + ' </h4><h4>' + itemX.description + ' </h4><h4>' + itemX.comment + ' </h4><h4>changes made by: '+ itemX.updatedBy+'</h4><p>full item data:</p><p>'+JSON.stringify(itemX)+'</p></body></html>';
+    packageEmail.subject = "[inventory] changes have been made to " + itemX.name;
+    packageEmail.HTMLbody += "here is the full item:<br/>";
+    packageEmail.HTMLbody += JSON.stringify(itemX) + "<br/>";
+    packageEmail.HTMLbody += "do you approve?<br/>";
+    packageEmail.HTMLbody += "<a href='http://127.0.0.1:55656/api/unStage/"+actionKey+"/true'>YES!</a>"
 
-    alert(JSON.stringify(packageEmail));
     $http.post('/api/sendEmail',packageEmail);
-  }//end itemChangeEmailGen
+  }//end itemChangeEmailGen ---------------
 
 
-//testing get single item
+  //testing get single item
   $http.post('/api/getItem', JSON.stringify({ uid:'7fd09d83-24df-4439-86b6-91e2c0245931'})).success(function(data){
     //success function
     testes = data; //WORKS!
   });
 
-}); // end itemCtrl
+}); // end itemCtrl #########
 
 
+//############################################################################################################################################################
+//GENERAL FUNCTIONS ############################################################################################################################################################
+//############################################################################################################################################################
 
-//GENERAL FUNCTIONS and DEFAULTS ---------------------------------------------------------
 function generateUID() {
   return 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
       var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
