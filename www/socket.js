9748ab8b31d8bcda7ebd00c5749662fa4dc6ec53
Steven Hein <steven.c.hein@gmail.com>
2014-11-14 18:15:09 -0500

    small fix

--- www/socket.js
+++
@@ -1,7 +0,0 @@
-socket.on('message', function(msg){
-	$('#socket-messages').append('<p>'+msg+'</p>');
-});
-
-socket.on('update', function(msg){
-	$('#socket-messages').append('<p>'+msg+'</p>');
-});
