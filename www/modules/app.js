ea7be15ec4731e0d9eb7f17f98328481942d8387
Steven Hein <steven.c.hein@gmail.com>
2015-09-19 14:55:03 -0400

    changed directory stucture for directives

--- www/modules/app.js
+++ www/modules/app.js
@@ -7,10 +7,11 @@ define([
     'app.ctrl',
     'app.config',
     'app.dir',
+    'toolbar/dir',
+    'editOwners/dir',
     'app.filter',
-
     'angular-ui-router'
-], function (angular, appSvc, appCtrl, appCfg, appDir, appFilt) {
+], function (angular, appSvc, appCtrl, appCfg, appDir, appDirToolbar, appDirEditOwners, appFilt) {
     'use strict';
 
     return angular.module('app', ['ui.router'])
@@ -18,5 +19,8 @@ define([
         .controller(appCtrl)
         .config(appCfg)
         .directive(appDir)
+        //modules
+        .directive(appDirToolbar)
+        .directive(appDirEditOwners)
         .filter(appFilt);
 });
