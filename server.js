e1b307e4f346a9650deda2ab9425e85900c9387a
Micha Gorelick <mynameisfiber@gmail.com>
2015-09-19 11:21:21 -0400

    first try at breaking apart

--- server.js
+++ server.js
@@ -61,67 +61,8 @@ if (!fs.existsSync('/vagrant/www/media/images')) {
 }
 //default item image:
 var defaultImage = 'images/default.jpg';
+var dbInfo = require("database/dbInfo");
 
-var dbInfo = {
-    formElements:['text', 'textarea', 'url'],
-    types:[
-      {
-        name:'tool',
-        color:{r:'76',g:'164',b:'84'},
-        formFields:[
-          {name:'need', type:'radio', options:['have','want'], default:'have'},
-          {name:'description',type:'textarea'}, 
-          {name:'location',type:'text'},
-          {name:'image search', type:'image-search'},
-          {name:'imageURL', type:'url'}
-        ]
-      },
-      {
-        name:'resource',
-        color:{r:'68',g:'114',b:'185'},
-        formFields:[
-          {name:'need', type:'radio', options:['have','want'], default:'have'},
-          {name:'description',type:'textarea'}, 
-          {name:'location',type:'text'},
-          {name:'image search', type:'image-search'},
-          {name:'imageURL', type:'url'}
-        ]
-      },
-      {
-        name:'project',
-        color:{r:'225',g:'135',b:'40'},
-        formFields:[
-          {name:'description',type:'textarea'},
-		  {name:'image search', type:'image-search'},
-          {name:'imageURL', type:'url'}
-        ]
-      }, 
-      {
-        name:'book',
-        color:{r:'190',g:'76',b:'57'},
-        formFields:[
-          {name:'need', type:'radio', options:['have','want'], default:'have'},
-          {name:'description',type:'textarea'},
-          {name:'image search', type:'image-search'},
-          {name:'imageURL', type:'url'}
-        ]
-      },
-      {
-        name:'event',
-        color:{r:'147',g:'81',b:'166'},
-        formFields:[
-          {name:'title', type:'text'},
-          {name:'description',type:'textarea'},
-          {name:'start', type:'text', default:'TBD'},
-          {name:'end', type:'text', default:'TBD'}
-        ]
-      },
-      {
-      	name:'deleted',
-      	color:{r:'225',g:'20',b:'20'}
-      }
-    ]
-};
 
 var proposalSecrets = {};
 //var members = [{name:'micha'},{name:'jackie'},{name:'mike'},{name:'coop'},{name:'steve', email:'steven.c.hein@gmail.com'}];
